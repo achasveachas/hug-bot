88287940be12b37979ed335375b82eba84194aa7
Yechiel K <contact@yechiel.me>
2020-02-20 22:02:05 -0500

    set up Twitter client

--- hug-bot.py
+++ hug-bot.py
@@ -1,7 +1,18 @@
 import time
 import urllib.request
-import giphy_client
 from os import environ
+import giphy_client
+import tweepy
+
+twitter_api_key = environ['twitter_api_key']
+twitter_api_secret = environ['twitter_api_secret']
+twitter_access_token = environ['twitter_access_token']
+twitter_access_secret = environ['twitter_access_secret']
+auth = tweepy.OAuthHandler(twitter_api_key, twitter_api_secret)
+auth.set_access_token(twitter_access_token, twitter_access_secret)
+api = tweepy.API(auth)
+
+api.update_status("Hello World")
 
 def download_random_gif():
     giphy_api = giphy_client.DefaultApi()
@@ -11,5 +22,3 @@ def download_random_gif():
     api_response = giphy_api.gifs_random_get(giphy_api_key, tag=tag, fmt=fmt)
     
     urllib.request.urlretrieve(api_response.data.image_url,'hug.gif')
-
-download_random_gif()
