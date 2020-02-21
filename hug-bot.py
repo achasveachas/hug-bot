da80f39766bd19dde164e058bfecc4b50e263669
Yechiel K <contact@yechiel.me>
2020-02-20 22:35:28 -0500

    Set up to tweet hug gifs

--- hug-bot.py
+++ hug-bot.py
@@ -1,5 +1,6 @@
 import time
 import urllib.request
+import os
 from os import environ
 import giphy_client
 import tweepy
@@ -11,8 +12,7 @@ twitter_access_secret = environ['twitter_access_secret']
 auth = tweepy.OAuthHandler(twitter_api_key, twitter_api_secret)
 auth.set_access_token(twitter_access_token, twitter_access_secret)
 api = tweepy.API(auth)
-
-api.update_status("Hello World")
+hug_filename = "hug.gif"
 
 def download_random_gif():
     giphy_api = giphy_client.DefaultApi()
@@ -21,4 +21,10 @@ def download_random_gif():
     fmt = 'json'
     api_response = giphy_api.gifs_random_get(giphy_api_key, tag=tag, fmt=fmt)
     
-    urllib.request.urlretrieve(api_response.data.image_url,'hug.gif')
+    urllib.request.urlretrieve(api_response.data.image_url, hug_filename)
+
+download_random_gif()
+gif_upload = api.media_upload(hug_filename)
+api.update_status("Hello World (with hug)", media_ids=[gif_upload.media_id])
+
+os.remove(hug_filename)
