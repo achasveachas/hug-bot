b06f0766ce25276b38cd8ffff714af62b4cc15d9
Yechiel K <contact@yechiel.me>
2020-02-22 23:13:08 -0500

    add flow to listen for mentions and reply

--- hug-bot.py
+++ hug-bot.py
@@ -23,12 +23,34 @@ def download_random_gif():
     
     urllib.request.urlretrieve(api_response.data.image_url, hug_filename)
 
-download_random_gif()
-# Twitter has a max upload size of 15MB
-while os.path.getsize(hug_filename) > 15728640:
+def check_mentions(since_id):
+    new_since_id = since_id
+    for tweet in tweepy.Cursor(api.mentions_timeline,
+        since_id=since_id).items():
+        new_since_id = max(tweet.id, new_since_id)
+        print()
+        tweet_gif(tweet.author.screen_name, tweet.id)
+    return new_since_id
+
+def tweet_gif(reply_to_user, reply_to_id):
     download_random_gif()
+    # Twitter has a max upload size of 15MB
+    while os.path.getsize(hug_filename) > 15000000:
+        download_random_gif()
 
-gif_upload = api.media_upload(hug_filename)
-api.update_status("This account is under construction, please excuse our appearance", media_ids=[gif_upload.media_id])
+    gif_upload = api.media_upload(hug_filename)
+    api.update_status(
+        status="@%s Have a hug!" % reply_to_user,
+        media_ids=[gif_upload.media_id],
+        in_reply_to_status_id=reply_to_id
+    )
+    os.remove(hug_filename)
 
-os.remove(hug_filename)
+def main():
+    since_id = 1231428395843702785
+    while True:
+        since_id = check_mentions(since_id)
+        time.sleep(10)
+
+if __name__ == "__main__":
+    main()
