85e928ddab0d5cf2ce31dfe25ca92116254502f7
Yechiel K <contact@yechiel.me>
2020-02-24 18:03:18 -0500

    minor refactor

--- hug-bot.py
+++ hug-bot.py
@@ -16,7 +16,7 @@ twitter_access_secret = environ['twitter_access_secret']
 auth = tweepy.OAuthHandler(twitter_api_key, twitter_api_secret)
 auth.set_access_token(twitter_access_token, twitter_access_secret)
 api = tweepy.API(auth)
-hug_filename = "hug.gif"
+gif_filename = "hug.gif"
 
 def download_random_gif():
     giphy_api = giphy_client.DefaultApi()
@@ -24,20 +24,20 @@ def download_random_gif():
     tag = 'hug'
     fmt = 'json'
     rating = 'g'
-    open(hug_filename, 'w')
+    open(gif_filename, 'w')
     # Twitter has a max upload size of 15MB
-    while os.path.getsize(hug_filename) == 0 or os.path.getsize(hug_filename) > 15000000:
+    while os.path.getsize(gif_filename) == 0 or os.path.getsize(gif_filename) > 15000000:
         api_response = giphy_api.gifs_random_get(giphy_api_key, tag=tag, fmt=fmt, rating=rating)
-        urllib.request.urlretrieve(api_response.data.image_url, hug_filename)
+        urllib.request.urlretrieve(api_response.data.image_url, gif_filename)
 
 def tweet_gif():
-    gif_upload = api.media_upload(hug_filename)
-    api.create_media_metadata(media_id=gif_upload.media_id, alt_text="randomly generated gif hopefully depicting a hug")
+    gif_upload = api.media_upload(gif_filename)
+    api.create_media_metadata(media_id=gif_upload.media_id, alt_text="randomly generated gif, hopefully depicting a hug")
     api.update_status(
         status=choice(STATUSES),
         media_ids=[gif_upload.media_id],
     )
-    os.remove(hug_filename)
+    os.remove(gif_filename)
 
 def main():
     while True:
