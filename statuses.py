fdc61ac06155ffcd3ebf644e70663e9cdf20b617
Yechiel K <contact@yechiel.me>
2020-02-27 13:26:48 -0500

    fix typo

--- statuses.py
+++ statuses.py
@@ -13,7 +13,7 @@ STATUSES = (
    "A hug a day keeps the doctor away.",
    "Need a hug?",
    "Make hugs, not war!",
-   "1. Give hugs.\n\n2. ...\n3. Profit?\n\n\n(Kidding of course! The hugs ARE the profit!)",
+   "1. Give hugs.\n2. ...\n3. Profit?\n\n\n(Kidding of course! The hugs ARE the profit!)",
    "Hugs.\n\n\n\nThat's it, that's the tweet.",
    "Nobody, nobody at all:\n\nMe: Here, have a hug!",
    "Hugging a friend makes you feel better, makes them feel better, and makes the world a better place!\n\n\nThanks for coming to my TED talk.",
