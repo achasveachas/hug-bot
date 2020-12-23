2a5ac88e21905efea06d0cda8e501fd24ded293b
Yechiel K <contact@yechiel.me>
2020-12-23 12:04:41 -0500

    add support for command line arguments.

--- hug-bot.py
+++ hug-bot.py
@@ -1,6 +1,7 @@
 import time
 import urllib.request
 import os
+import sys
 from os import environ
 from datetime import datetime
 from random import choice
@@ -23,10 +24,12 @@ def twitter_api():
 def download_random_gif():
     giphy_api = giphy_client.DefaultApi()
     giphy_api_key = environ['giphy_api_key']
-    tag = 'hug'
+    tags = ['hug']
+    tags.extend(sys.argv[1:])
+    tag = ' '.join(tags)
     fmt = 'json'
     open(gif_filename, 'w')
-    while os.path.getsize(gif_filename) == 0 or os.path.getsize(gif_filename) > 14649 * 1024:
+    while os.path.getsize(gif_filename) == 0 or os.path.getsize(gif_filename) > 14648 * 1024:
         api_response = giphy_api.gifs_random_get(giphy_api_key, tag=tag, fmt=fmt)
         urllib.request.urlretrieve(api_response.data.image_url, gif_filename)
     print("Downloaded GIF ID: {}, GIF URL: {}".format(api_response.data.id, api_response.data.image_url))
