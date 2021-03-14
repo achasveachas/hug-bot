f7ea36bc2674c80c26cb3e6956c56a1f632c0780
Yechiel K <contact@yechiel.me>
2021-03-14 14:23:21 -0400

    add bot that replies to people needing hugs with hugs

---
+++ hug-replier.py
@@ -0,0 +1,24 @@
+import giphy
+import os
+from tweepy_client import twitter_api
+
+gif_filename = "hug.gif"
+
+api = twitter_api()
+tweets = api.search(q="need a hug", result_type="recent", count=100)
+
+for i, tweet in enumerate(tweets, start=1):
+    try:
+        giphy.download_random_gif()
+        gif_upload = api.media_upload(gif_filename)
+        api.create_media_metadata(media_id=gif_upload.media_id, alt_text="randomly generated gif, hopefully depicting a hug. ")
+        status = api.update_status(
+            in_reply_to_status_id=tweet.id,
+            auto_populate_reply_metadata=True,
+            media_ids=[gif_upload.media_id],
+        )
+        print(f"Replied to tweet number {i}, id: {tweet.id}")
+    except:
+        print(f"An error occured while replying to tweet id: {tweet.id}")
+    finally:
+        os.remove(gif_filename)
