1c29b8779f6917b85fa6ab5edcd6de941012c996
Yechiel K <contact@yechiel.me>
2021-03-17 23:22:51 -0400

    this time duplicate retweets should work!

--- hug-replier.py
+++ hug-replier.py
@@ -6,23 +6,25 @@ gif_filename = "hug.gif"
 
 api = twitter_api()
 tweets = api.search(q="need a hug", result_type="recent", count=100)
-tweet_ids = [tweet.id for tweet in tweets]
+tweet_ids = set()
+for tweet in tweets:
+    if hasattr(tweet, "retweeted_status"):
+        tweet_ids.add(tweet.retweeted_status.id)
+    else:
+        tweet_ids.add(tweet.id)
 
-for i, tweet in enumerate(tweets, start=1):
-    if hasattr(tweet, "retweeted_status") and tweet.retweeted_status.id in tweet_ids:
-        print(f"skipping retweet {tweet.id}")
-        continue
+for i, id in enumerate(tweet_ids, start=1):
     try:
         giphy.download_random_gif()
         gif_upload = api.media_upload(gif_filename)
         api.create_media_metadata(media_id=gif_upload.media_id, alt_text="randomly generated gif, hopefully depicting a hug. ")
         api.update_status(
-            in_reply_to_status_id=tweet.id,
+            in_reply_to_status_id=id,
             auto_populate_reply_metadata=True,
             media_ids=[gif_upload.media_id],
         )
-        print(f"Replied to tweet number {i}, id: {tweet.id}")
+        print(f"Replied to tweet number {i}, id: {id}")
     except Exception as e:
-        print(f"An error occured while replying to tweet id: {tweet.id}\n{e.message}")
+        print(f"An error occured while replying to tweet id: {id}\n{e}")
     finally:
         os.remove(gif_filename)
