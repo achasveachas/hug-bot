a20a347f465ed8d8efe267590429554c65cf9069
Yechiel Kalmenson <ykalmenson@vmware.com>
2024-06-26 13:35:42 -0400

    add blusky support

--- hug-bot.py
+++ hug-bot.py
@@ -1,6 +1,6 @@
 import os
 from random import choice
-from tweepy_client import twitter_api
+from atproto import Client
 import giphy
 
 from statuses import STATUSES
@@ -8,12 +8,11 @@ from statuses import STATUSES
 giphy.download_random_gif()
 gif_filename = "hug.gif"
 
-api = twitter_api()
-gif_upload = api.media_upload(gif_filename, chunked=True, media_category="tweet_gif")
-api.create_media_metadata(media_id=gif_upload.media_id, alt_text="randomly generated gif, hopefully depicting a hug. ")
-status = api.update_status(
-    status=choice(STATUSES),
-    media_ids=[gif_upload.media_id],
-)
-print("Sent Tweet ID: " + status.id_str)
-os.remove(gif_filename)
+client = Client()
+client.login(os.environ['BLUESKY_USERNAME'], os.environ['BLUESKY_PASSWORD'])
+
+with open(gif_filename, 'rb') as f:
+    img_data = f.read()
+    client.send_image(text='test', image=img_data, image_alt='Img alt')
+
+#os.remove(gif_filename)
